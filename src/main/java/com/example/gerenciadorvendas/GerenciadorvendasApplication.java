1f867ccbaa38aee2f59524f018d07ac3d81ec398
Thullyoo <123775158+Thullyoo@users.noreply.github.com>
2024-03-11 16:12:44 -0300

    feat: first commit

---
+++ src/main/java/com/example/gerenciadorvendas/GerenciadorvendasApplication.java
@@ -0,0 +1,13 @@
+package com.example.gerenciadorvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorvendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gerenciadorvendas/GerenciadorvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerenciadorvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
